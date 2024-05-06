0e0a230ed5714a3c0187bf5f00134cba97e118b2
Mohammad Zidan Yohanza <100187385+ZidanYohanza@users.noreply.github.com>
2024-05-06 18:31:03 +0700

    Delete vue-pplTerserah directory

--- vue-pplTerserah/src/components/HeadFoot/Footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <footer class="bg-bottom bg-gradient-to-b  from-[#9CA69C] to-[#8B968B] font-[Poppins] text-white shadow">
-    <div class="flex items-center justify-around h-32 ">
-      <img class="lg:h-20 md:h-16 h-10 hover:scale-105" src="/public/Logo.png" alt="">
-      <p class="lg:text-2xl md:text-[24px] text-[13px]">© 2024 TERSERAH Inc. All rights reserved.</p>
-      </div>
-  </footer>
-</template>
