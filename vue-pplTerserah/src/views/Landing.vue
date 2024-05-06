6b4f04ab870da1ae077534d4c337f32e33a2fe70
ZidanYohanza <zidan1juli@gmail.com>
2024-05-06 18:26:14 +0700

    Landing Page, Header&Footer, Login&Register CLEAR

---
+++ vue-pplTerserah/src/views/Landing.vue
@@ -0,0 +1,22 @@
+<script>
+    import LandTags from '../components/LandComp/LandTag.vue';
+    import LandRecs from '../components/LandComp/LandRec.vue';
+    import LandPromos from '../components/LandComp/LandPromo.vue';
+    import LandArtikels from '../components/LandComp/LandArtikel.vue';
+
+    export default {
+        components : {
+            LandTags,
+            LandRecs,
+            LandPromos,
+            LandArtikels
+        }
+    }
+</script>
+
+<template>
+    <LandTags />
+    <LandRecs />
+    <LandPromos />
+    <LandArtikels />
+</template>
---
+++ vue-pplTerserah/src/views/Registrasi.vue
@@ -0,0 +1,16 @@
+<script>
+    import RegisTag from '../components/RegisComp/RegisTag.vue';
+    import RegisForm from '../components/RegisComp/RegisForm.vue';
+
+    export default {
+        components : {
+            RegisForm,
+            RegisTag
+        }
+    }
+</script>
+
+<template>
+    <RegisTag />
+    <RegisForm />
+</template>
